b0671e695d3ac66a783d1b33ce4b191ad1a2f2f3
S-BCH <S-BCH@users.noreply.github.com>
2023-02-05 17:36:38 +0100

    Finish

--- src/app/appareil/appareil.component.ts
+++ src/app/appareil/appareil.component.ts
@@ -1,7 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { Appareil } from '../appareil/Appareil'
 
-
 @Component({
   selector: 'app-appareil',
   templateUrl: './appareil.component.html',
